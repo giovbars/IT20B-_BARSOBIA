37fba667c6a105219eeca115bb82da14385e74ec
giovbars <giovannybarsobia04@gmail.com>
2024-10-17 15:43:53 +0800

    Add files via upload

---
+++ SimpleQueueExample.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplequeue;
+
+/**
+ *
+ * @author ITLAB1-PC23-STUDENT
+ */
+public class SIMPLEQUEUE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ linkedlist.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlist1;
+
+/**
+ *
+ * @author ITLAB1-PC23-STUDENT
+ */
+public class LINKEDLIST1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
